e65af64bef29dbd9dfd07936ae049284538ba9f5
Donovan Harrison <donjuan0802@gmail.com>
2025-03-31 21:00:49 -0500

    feat: initial project setup

---
+++ src/test/java/io/github/dhar135/unit_converter/UnitConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dhar135.unit_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
