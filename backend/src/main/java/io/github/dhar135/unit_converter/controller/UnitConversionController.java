613b3df5b93f7c8f9c8df432f1f95beec4f7a1ff
Donovan Harrison <donjuan0802@gmail.com>
2025-04-03 01:18:02 -0500

    feat: add ConversionResponse record and update UnitConversionController to return structured responses

--- backend/src/main/java/io/github/dhar135/unit_converter/controller/UnitConversionController.java
+++ backend/src/main/java/io/github/dhar135/unit_converter/controller/UnitConversionController.java
@@ -5,14 +5,15 @@ import io.github.dhar135.unit_converter.enums.TempUnit;
 import io.github.dhar135.unit_converter.enums.WeightUnit;
 import io.github.dhar135.unit_converter.service.UnitConversionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api/convert")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UnitConversionController {
 
     private final UnitConversionService unitConversionService;
@@ -21,35 +22,37 @@ public class UnitConversionController {
         this.unitConversionService = unitConversionService;
     }
 
-
     @GetMapping("/length")
-    public ResponseEntity<Double> convertLength(@RequestParam("value") double value, @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
+    public ResponseEntity<ConversionResponse> convertLength(@RequestParam("value") double value,
+            @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
 
         LengthUnit fromUnit = LengthUnit.fromSymbol(fromUnitString);
-        LengthUnit toUnit =  LengthUnit.fromSymbol(toUnitString);
+        LengthUnit toUnit = LengthUnit.fromSymbol(toUnitString);
 
         double result = unitConversionService.convertLength(value, fromUnit, toUnit);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(new ConversionResponse(result, toUnitString));
     }
 
     @GetMapping("/weight")
-    public ResponseEntity<Double> convertWeight(@RequestParam("value") double value, @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
+    public ResponseEntity<ConversionResponse> convertWeight(@RequestParam("value") double value,
+            @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
 
         WeightUnit fromUnit = WeightUnit.fromSymbol(fromUnitString);
-        WeightUnit toUnit =  WeightUnit.fromSymbol(toUnitString);
+        WeightUnit toUnit = WeightUnit.fromSymbol(toUnitString);
 
         double result = unitConversionService.convertWeight(value, fromUnit, toUnit);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(new ConversionResponse(result, toUnitString));
     }
 
     @GetMapping("/temperature")
-    public ResponseEntity<Double> convertTemperature(@RequestParam("value") double value, @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
+    public ResponseEntity<ConversionResponse> convertTemperature(@RequestParam("value") double value,
+            @RequestParam("from") String fromUnitString, @RequestParam("to") String toUnitString) {
 
         TempUnit fromUnit = TempUnit.fromSymbol(fromUnitString);
-        TempUnit toUnit =  TempUnit.fromSymbol(toUnitString);
+        TempUnit toUnit = TempUnit.fromSymbol(toUnitString);
 
         double result = unitConversionService.convertTemperature(value, fromUnit, toUnit);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(new ConversionResponse(result, toUnitString));
     }
 
 }
